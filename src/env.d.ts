c753604b48268d587230107f18d999097055d4cc
张智皓 <zhang948498@163.com>
2025-10-20 18:29:56 +0800

    feat: 🎸 初始化代码

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_BASE_SHOP_API: string;
+  readonly VITE_BASE_SHOP_WSAPI: string;
+  // 更多环境变量...
+}
+declare const __APP_BUILD_TIME__: number;
---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import { useTitle } from '@vueuse/core';
+import type { RouteRecordRaw } from 'vue-router';
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    title?: string;
+    keepAlive?: boolean;
+  }
+}
+/**
+ * 自动导入 modules 下的所有路由
+ */
+const moduleFiles: Record<`./modules/*${string}.ts`, RouteRecordRaw[]> = import.meta.glob('./modules/**/*.ts', { import: 'default', eager: true });
+const modules: Array<RouteRecordRaw> = Object.values(moduleFiles).flat();
+
+export const routes: Array<RouteRecordRaw> = [...modules];
+const router = createRouter({
+  history: createWebHistory(import.meta.env.VITE_BASE_URL ?? '/'),
+  routes,
+});
+const title = useTitle();
+
+// 更改路由后  更改title
+router.afterEach(to => {
+  title.value = to.meta?.title ?? `三方-${to.name?.toString()}`;
+});
+export default router;
